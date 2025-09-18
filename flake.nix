7b99eacee5c6c328635c31978c79a8f50ef6bad3
nick <nicc.gemm@gmail.com>
2025-09-18 12:03:03 +0200

    Changed template flakes to be applied for each system

--- flake.nix
+++ flake.nix
@@ -1,7 +1,20 @@
 {
 	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { ... }: {
+  outputs = { nixpkgs, ... }: {
+    lib = {
+      mkFlake = (fn:
+        let
+          systems = [
+            "aarch64-linux"
+            "x86_64-linux"
+          ];
+          lib = (import nixpkgs {system="x86_64-linux";}).lib;
+        in
+          builtins.foldl' lib.attrsets.recursiveUpdate {} (builtins.map fn systems)
+      );
+    };
+
 		templates = {
 			zig.path = templates/zig;
 			haskell.path = templates/haskell;
--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -1,26 +1,33 @@
 {
-  inputs.nixpkgs.url = "nixpkgs";
-  outputs = { self, nixpkgs }: let
-    system = "aarch64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {};
-    };
-    name = "haskell-project";
-  in {
-    packages.${system} = {
-      default = self.packages.${system}.${name};
-
-      ${name} = pkgs.haskellPackages.callCabal2nix name ./. {};
-    };
-    devShell = pkgs.haskellPackages.shellFor {
-      packages = p: [self.packages.${system}.${name}];
-      withHoogle = true;
-      buildInputs = with pkgs.haskellPackages; [
-        haskell-language-server
-        ghcid
-        cabal-install
-      ];
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    nix-utils = {
+      url = "github:vivAnicc/nix-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nix-utils, nixpkgs }:
+    nix-utils.lib.mkFlake (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {};
+      };
+      name = "haskell-project";
+    in {
+      packages.${system} = {
+        default = self.packages.${system}.${name};
+
+        ${name} = pkgs.haskellPackages.callCabal2nix name ./. {};
+      };
+      devShell = pkgs.haskellPackages.shellFor {
+        packages = p: [self.packages.${system}.${name}];
+        withHoogle = true;
+        buildInputs = with pkgs.haskellPackages; [
+          haskell-language-server
+          ghcid
+          cabal-install
+        ];
+      };
+    });
 }
