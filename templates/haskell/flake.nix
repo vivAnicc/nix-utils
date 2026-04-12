02adfb7210ca62b3381044df796732fd39474d72
nick <nicc.gemm@gmail.com>
2026-04-12 17:28:46 +0200

    Fixed haskell template

--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -9,19 +9,34 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-      name = "haskell";
-    in rec {
-      packages.${system}.${name} = devShell.${system};
-      
-      devShell.${system} = pkgs.haskellPackages.developPackage {
-        root = ./.;
-        returnShellEnv = true;
 
-        modifier = drv:
-          pkgs.haskell.lib.addBuildTools drv [
-            pkgs.haskellPackages.cabal-install
-            pkgs.haskellPackages.ghcid
-          ];
+      name = "haskell-project";
+
+      opts = {
+        root = ./.;
+        inherit name;
+        withHoogle = true;
       };
+
+      nvim = utils.lib.mkNeovim {
+        inherit system;
+# hls is somehow broken, so I just don't include it
+        # extraPkgs = [ pkgs.haskellPackages.haskell-language-server ];
+        # lsp = [ "hls" ];
+      };
+    in {
+      packages.${system} = rec {
+        default = pkgs.haskellPackages.developPackage opts;
+        ${name} = default;
+      };
+
+      devShell.${system} = pkgs.haskellPackages.developPackage (opts // {
+        returnShellEnv = true;
+        modifier = drv: pkgs.haskell.lib.addBuildTools drv [
+          pkgs.haskellPackages.cabal-install
+          pkgs.haskellPackages.ghcid
+          nvim
+        ];
+      });
     });
 }
